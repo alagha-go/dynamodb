6b7f35870adde7ef55f0283cffee300c9a1b861f
alagha-go <abdihakimosman1480@gmail.com>
2023-08-11 12:01:22 +0300

    impl some necessary traits

--- src/primitives/binary.rs
+++ src/primitives/binary.rs
@@ -1,6 +1,6 @@
 use serde::{Deserializer, Serializer};
 use serde::{Serialize, Deserialize};
-use super::BASE64;
+use super::{BASE64, Rc};
 use base64::Engine;
 
 /// holds a `Vec<u8>` that would be turned into the `B` AttributeValue of dynamodb
@@ -20,4 +20,36 @@ pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error>
     let base64 = String::deserialize(d)?;
     let bytes = BASE64.decode(base64.as_bytes()).map_err(|e| serde::de::Error::custom(e))?;
     Ok(bytes.into())
+}
+
+impl From<Vec<u8>> for Binary {
+    fn from(value: Vec<u8>) -> Self {
+        Binary(value)
+    }
+}
+
+impl<const SIZE: usize> From<[u8; SIZE]> for Binary {
+    fn from(value: [u8; SIZE]) -> Self {
+        let value = value.to_vec();
+        Binary(value)
+    }
+}
+
+impl From<&[u8]> for Binary {
+    fn from(value: &[u8]) -> Self {
+        let value = value.into_iter().map(|byte|{*byte}).collect::<Vec<_>>();
+        Binary(value)
+    }
+}
+
+impl<T: Into<Binary>> From<Box<T>> for Binary {
+    fn from(value: Box<T>) -> Self {
+        value.into()
+    }
+}
+
+impl<T: Into<Binary>> From<Rc<T>> for Binary {
+    fn from(value: Rc<T>) -> Self {
+        value.into()
+    }
 }
--- src/primitives/item.rs
+++ src/primitives/item.rs
@@ -6,3 +6,17 @@ pub trait Item {
     /// convert the given type into a key value pair
     fn item(&self) -> HashMap<String, AttributeValue>;
 }
+
+
+impl Item for HashMap<String, AttributeValue> {
+    fn item(&self) -> HashMap<String, AttributeValue> {
+        self.clone()
+    }
+}
+
+
+impl Item for BTreeMap<String, AttributeValue> {
+    fn item(&self) -> HashMap<String, AttributeValue> {
+        self.into_iter().map(|(key, value)|{(key.to_owned(), value.to_owned())}).collect()
+    }
+}
